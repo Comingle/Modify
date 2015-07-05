9b4036a39a435a8d18644f211a9f48224665c9f7
Patrick <pdougall1@gmail.com>
2015-07-05 12:01:17 -0400

    Begins slider-input and removes old slider component

--- app/components/sliding-value.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-
-  change: function (val) {
-    this.sendAction('changeMotor', this.get('motorNum'), val.target.valueAsNumber);
-  }
-
-});
--- tests/unit/components/slider-input-test.js
+++ tests/unit/components/slider-input-test.js
@@ -3,7 +3,7 @@ import {
   test
 } from 'ember-qunit';
 
-moduleForComponent('sliding-value', {
+moduleForComponent('slider-input', {
   // Specify the other units that are required for this test
   // needs: ['component:foo', 'helper:bar']
 });
